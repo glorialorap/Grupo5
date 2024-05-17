40a05ad3f62f8a045adf3da72df59e118736dc0f
AnaFer28 <161053287+AnaFer28@users.noreply.github.com>
2024-05-17 15:02:29 -0500

    Update App.java

--- Taller/App.java
+++ Taller/App.java
@@ -7,8 +7,8 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Figuras figuras = new Figuras(0,0,0);
-        Hexagono hexagono = new Hexagono(0,0,0,0,0);
-        Pentágono pentágono = new Pentágono(0, 0, 0, 0, 0);
+        Hexagono hexagono = new Hexagono(0,0);
+        Pentagono pentagono = new Pentagono(0, 0);
         
         // Pentágono
         System.out.println("Ingrese el lado del pentágono:");
@@ -16,8 +16,8 @@ public class App {
         System.out.println("Ingrese la apotema del pentágono:");
         double apotemaP = inputPositivo(scanner);
 
-        pentágono.setLadoP(ladoP);
-        pentágono.setApotemaP(apotemaP);
+        pentagono.setLadoP(ladoP);
+        pentagono.setApotemaP(apotemaP);
     
         Function<Double, Double> perimetroPentagono = (lado) -> 5 * ladoP;
         System.out.println("Perímetro del Pentágono: " + perimetroPentagono.apply(ladoP));
@@ -26,19 +26,19 @@ public class App {
         System.out.println("Área del Pentágono: " + areaPentagono.apply(perimetroPentagono.apply(ladoP), apotemaP));
 
         // Hexagono
-        System.out.println("Ingrese el lado del pentágono:");
+        System.out.println("Ingrese el lado del Hexágono:");
         double lado = inputPositivo(scanner);
-        System.out.println("Ingrese la apotema del pentágono:");
+        System.out.println("Ingrese la apotema del Hexágono:");
         double apotemaH = inputPositivo(scanner);
 
         hexagono.setLado(lado);
         hexagono.setApotemaH(apotemaH);
 
         Function<Double, Double> perimetroHexagono = (lad) -> 6 * lado;
-        System.out.println("Perímetro del Pentágono: " + perimetroHexagono.apply(lado));
+        System.out.println("Perímetro del Hexágono: " + perimetroHexagono.apply(lado));
 
         BiFunction<Double, Double, Double> areaHexagono = (perimetro, apotema) -> (perimetro * apotema) / 2;
-        System.out.println("Área del Pentágono: " + areaHexagono.apply(perimetroHexagono.apply(lado), apotemaH));
+        System.out.println("Área del Hexágono: " + areaHexagono.apply(perimetroHexagono.apply(lado), apotemaH));
 
     }
 
