adeddd096e2d69d441d54688201f936a04076339
AnaFer28 <161053287+AnaFer28@users.noreply.github.com>
2024-05-17 11:33:21 -0500

    Update Hexagono.java

--- Taller/Hexagono.java
+++ Taller/Hexagono.java
@@ -1,1 +1,30 @@
+public class Hexagono {
+    private int lado;
+    private double apotemaH;
+    public Hexagono() {
+    }
+    public Hexagono(int lado, double apotemaH) {
+        this.lado = lado;
+        this.apotemaH = apotemaH;
+    }
+    public int getLado() {
+        return lado;
+    }
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+    public double getApotemaH() {
+        return apotemaH;
+    }
+    public void setApotemaH(double apotemaH) {
+        this.apotemaH = apotemaH;
+    }
+    @Override
+    public String toString() {
+        return "Hexagono [lado=" + lado + ", apotemaH=" + apotemaH + "]";
+    }
 
+
+    
+
+}
