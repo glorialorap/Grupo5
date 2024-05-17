05be7096806dedd03ccd0f2e9903965964571036
AnaFer28 <161053287+AnaFer28@users.noreply.github.com>
2024-05-17 11:33:58 -0500

    Create Paralelo.java

---
+++ Taller/Paralelo.java
@@ -0,0 +1,30 @@
+public class Paralelo {
+    private double base;
+    private double altura;
+    public Paralelo() {
+    }
+    public Paralelo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    public double getBase() {
+        return base;
+    }
+    public void setBase(double base) {
+        this.base = base;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    @Override
+    public String toString() {
+        return "Paralelo [base=" + base + ", altura=" + altura + "]";
+    }
+
+
+    
+    
+}
