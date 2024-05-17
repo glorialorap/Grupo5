e1dd135aac5ceda86abc733cf4a6e16835bd7fdc
AnaFer28 <161053287+AnaFer28@users.noreply.github.com>
2024-05-17 11:04:24 -0500

    Update Figuras.java

---
+++ Taller/Figuras.java
@@ -0,0 +1,50 @@
+public class Figuras{
+  private Double Area;
+  private Double Perimetro;
+  private Double Promedio; 
+  
+  public Figuras(){
+    
+  }
+  public Figuras(double Area, double Perimetro, double Promedio ) {
+        this.Area = Area;
+        this.Perimetro = Perimetro;
+        this.Promedio = Promedio;
+  }
+  public double getArea() {
+        return Area;
+    }
+
+    public void setArea(Double Area) {
+        this.Area = Area;
+    }
+
+    public Double getPerimetro() {
+        return Perimetro;
+    }
+
+    public void setPerimetro(Double Perimetro) {
+        this.Perimetro = perimetro;
+    }
+
+    public double getPromedio() {
+        return Promedio;
+    }
+
+    public void setPromedio(double Promedio) {
+        this.Promedio = Promedio;
+    }
+}
+   
+
+
+
+
+
+
+
+
+
+  
+  
+}
