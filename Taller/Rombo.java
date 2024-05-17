1c94878e16712c60d441aa3012246d04fdc4a754
AnaFer28 <161053287+AnaFer28@users.noreply.github.com>
2024-05-17 11:34:30 -0500

    Create Rombo.java

---
+++ Taller/Rombo.java
@@ -0,0 +1,37 @@
+public class Rombo {
+    private int ladoo;
+    private double DMay;
+    private double dmen;
+    public Rombo() {
+    }
+    public Rombo(int ladoo, double dMay, double dmen) {
+        this.ladoo = ladoo;
+        DMay = dMay;
+        this.dmen = dmen;
+    }
+    public int getLadoo() {
+        return ladoo;
+    }
+    public void setLadoo(int ladoo) {
+        this.ladoo = ladoo;
+    }
+    public double getDMay() {
+        return DMay;
+    }
+    public void setDMay(double dMay) {
+        DMay = dMay;
+    }
+    public double getDmen() {
+        return dmen;
+    }
+    public void setDmen(double dmen) {
+        this.dmen = dmen;
+    }
+    @Override
+    public String toString() {
+        return "Rombo [ladoo=" + ladoo + ", DMay=" + DMay + ", dmen=" + dmen + "]";
+    }
+
+    
+    
+}
